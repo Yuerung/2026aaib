5ccaca7087fe83db760a912b04fffcde7bf64ed2
Yuerung <14751255@me.mcu.edu.tw>
2026-05-20 11:53:00 +0800

    week13

---
+++ week13/week13-5c_1.cpp
@@ -0,0 +1,12 @@
+//week13-5c_1.cpp SOIT106_ADVANCE_009
+#include <stdio.h>
+int main()
+{
+    int N;
+    scanf("%d",&N);
+    int ans=0;
+    while(N>0){
+       ans=ans*10+N%10;
+       N=N/10;
+    }
+    printf("%d\n",ans);
