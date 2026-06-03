da2b130a79e3037d787eb75b87ab6d43428c76f9
Yuerung <14751255@me.mcu.edu.tw>
2026-06-03 11:51:38 +0800

    week15

---
+++ week15/wee15-2a.cpp
@@ -0,0 +1,13 @@
+//wee15-2a.cpp SOIT106_ADVANCE_010
+#include <stdio.h>
+int main()
+{
+    int N;
+    sanf("%d",&N);
+
+    for (int i=0;i<N;i++){
+       scnaf("%d",&a[i])'
+       printf("%d",a[i]*a[i]);
+    }
+    printf("\n");
+}
